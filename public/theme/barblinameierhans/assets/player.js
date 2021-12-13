f4510ecbbccb880b6f3c74bfa7722a53a1fb0423
Adrian Demleitner <adrian@thgie.ch>
2021-12-13 17:01:09 +0100

    fixed some audio player stuff

--- public/theme/barblinameierhans/assets/player.js
+++ public/theme/barblinameierhans/assets/player.js
@@ -4,66 +4,70 @@
 // - Be able to grab a custom title instead of "Music Song"
 // - Hover over sliders to see preview of timestamp/volume change
 
-const audioPlayer = document.querySelector(".audio-player");
-const audio = new Audio(audioPlayer.data.file);
-//credit for song: Adrian kreativaweb@gmail.com
+let audio_players = document.querySelectorAll('.audio-player');
 
-audio.addEventListener(
-  "loadeddata",
-  () => {
-    audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum(
-      audio.duration
-    );
-    audio.volume = .75;
-  },
-  false
-);
+audio_players.forEach((el) => {
+  const audioPlayer = el;
+  const audio = new Audio(audioPlayer.data.file);
+  //credit for song: Adrian kreativaweb@gmail.com
 
-//click on timeline to skip around
-const timeline = audioPlayer.querySelector(".timeline");
-timeline.addEventListener("click", e => {
-  const timelineWidth = window.getComputedStyle(timeline).width;
-  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
-  audio.currentTime = timeToSeek;
-}, false);
-
-//check audio percentage and update time accordingly
-setInterval(() => {
-  const progressBar = audioPlayer.querySelector(".progress");
-  progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
-  audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum(
-    audio.currentTime
+  audio.addEventListener(
+    "loadeddata",
+    () => {
+      audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum(
+        audio.duration
+      );
+      audio.volume = .75;
+    },
+    false
   );
-}, 500);
 
-//toggle between playing and pausing on button click
-const playBtn = audioPlayer.querySelector(".controls .toggle-play");
-playBtn.addEventListener(
-  "click",
-  () => {
-    if (audio.paused) {
-      playBtn.classList.remove("play");
-      playBtn.classList.add("pause");
-      audio.play();
-    } else {
-      playBtn.classList.remove("pause");
-      playBtn.classList.add("play");
-      audio.pause();
-    }
-  },
-  false
-);
+  //click on timeline to skip around
+  const timeline = audioPlayer.querySelector(".timeline");
+  timeline.addEventListener("click", e => {
+    const timelineWidth = window.getComputedStyle(timeline).width;
+    const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+    audio.currentTime = timeToSeek;
+  }, false);
 
-//turn 128 seconds into 2:08
-function getTimeCodeFromNum(num) {
-  let seconds = parseInt(num);
-  let minutes = parseInt(seconds / 60);
-  seconds -= minutes * 60;
-  const hours = parseInt(minutes / 60);
-  minutes -= hours * 60;
+  //check audio percentage and update time accordingly
+  setInterval(() => {
+    const progressBar = audioPlayer.querySelector(".progress");
+    progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+    audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum(
+      audio.currentTime
+    );
+  }, 500);
 
-  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
-  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+  //toggle between playing and pausing on button click
+  const playBtn = audioPlayer.querySelector(".controls .toggle-play");
+  playBtn.addEventListener(
+    "click",
+    () => {
+      if (audio.paused) {
+        playBtn.classList.remove("play");
+        playBtn.classList.add("pause");
+        audio.play();
+      } else {
+        playBtn.classList.remove("pause");
+        playBtn.classList.add("play");
+        audio.pause();
+      }
+    },
+    false
+  );
+
+  //turn 128 seconds into 2:08
+  function getTimeCodeFromNum(num) {
+    let seconds = parseInt(num);
+    let minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+    const hours = parseInt(minutes / 60);
+    minutes -= hours * 60;
+
+    if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+    return `${String(hours).padStart(2, 0)}:${minutes}:${String(
     seconds % 60
   ).padStart(2, 0)}`;
-}
+  }
+})
