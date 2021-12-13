11bc434858cd6d60d3409ec1bf8fa754634ae035
Adrian Demleitner <adrian@thgie.ch>
2021-12-13 16:55:49 +0100

    added some custom styling

--- public/theme/barblinameierhans/assets/player.js
+++ public/theme/barblinameierhans/assets/player.js
@@ -5,12 +5,8 @@
 // - Hover over sliders to see preview of timestamp/volume change
 
 const audioPlayer = document.querySelector(".audio-player");
-const audio = new Audio(
-  "https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/backsound.mp3"
-);
+const audio = new Audio(audioPlayer.data.file);
 //credit for song: Adrian kreativaweb@gmail.com
-
-console.dir(audio);
 
 audio.addEventListener(
   "loadeddata",
@@ -30,15 +26,6 @@ timeline.addEventListener("click", e => {
   const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
   audio.currentTime = timeToSeek;
 }, false);
-
-//click volume slider to change volume
-const volumeSlider = audioPlayer.querySelector(".controls .volume-slider");
-volumeSlider.addEventListener('click', e => {
-  const sliderWidth = window.getComputedStyle(volumeSlider).width;
-  const newVolume = e.offsetX / parseInt(sliderWidth);
-  audio.volume = newVolume;
-  audioPlayer.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
-}, false)
 
 //check audio percentage and update time accordingly
 setInterval(() => {
@@ -66,18 +53,6 @@ playBtn.addEventListener(
   },
   false
 );
-
-audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
-  const volumeEl = audioPlayer.querySelector(".volume-container .volume");
-  audio.muted = !audio.muted;
-  if (audio.muted) {
-    volumeEl.classList.remove("icono-volumeMedium");
-    volumeEl.classList.add("icono-volumeMute");
-  } else {
-    volumeEl.classList.add("icono-volumeMedium");
-    volumeEl.classList.remove("icono-volumeMute");
-  }
-});
 
 //turn 128 seconds into 2:08
 function getTimeCodeFromNum(num) {
